889682a1cd71727d29f71c2c82d1218918ae1f0c
Jevone Barrett <jevonebarrett@gmail.com>
2022-06-30 12:39:29 -0400

    Initial Commit

---
+++ War- Challenge/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  War- Challenge
+//
+//  Created by Jevone Barrett on 6/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+
+            Image ("background")
+                .ignoresSafeArea()
+    
+            VStack {
+                
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                Spacer()
+                Image("card2")
+                Spacer()
+                Image("card3")
+                Spacer()
+            }
+                Spacer()
+                    Image("dealbutton")
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                        }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                        }
+                    Spacer()
+                    }
+                Spacer()
+                }
+    }
+}
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War- Challenge/War__ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  War__ChallengeApp.swift
+//  War- Challenge
+//
+//  Created by Jevone Barrett on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct War__ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
