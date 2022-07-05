2fbb07400a0d80f22ac995b6073395311d8a94a9
Jevone Barrett <jevonebarrett@gmail.com>
2022-07-05 17:35:07 -0400

    Added a launchScreen and new logo

--- War- Challenge/ContentView.swift
+++ War- Challenge/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
         ZStack {
 
@@ -21,13 +27,39 @@ struct ContentView: View {
                 Spacer()
                 HStack {
                 Spacer()
-                Image("card2")
+                Image(playerCard)
                 Spacer()
-                Image("card3")
+                Image(cpuCard)
                 Spacer()
             }
                 Spacer()
+                
+                Button(action: {
+                    
+                    // Generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // Update the cards
+                    playerCard = "card" + String (playerRand)
+                    cpuCard = "card" + String (cpuRand)
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    else if cpuRand > playerRand{
+                        cpuScore += 1
+                    }
+                    else {
+                        cpuScore += 1
+                    }
+                    //  Update the score
+                    
+                }, label: {
                     Image("dealbutton")
+                    
+                })
+                
                 Spacer()
                 
                 HStack{
@@ -37,7 +69,7 @@ struct ContentView: View {
                             .font(.headline)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                         }
@@ -47,7 +79,7 @@ struct ContentView: View {
                             .font(.headline)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                         }
